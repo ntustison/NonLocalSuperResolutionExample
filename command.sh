b5329d07fbbc731b751262cf40f1d81ceb753755
Nick Tustison <ntustison@gmail.com>
2017-03-13 13:15:59 -0700

    ENH: Updating command to reflect class changes.

--- command.sh
+++ command.sh
@@ -1,6 +1,8 @@
 
 # As a test, try to recreate from original image
 
+
+
 NonLocalSuperResolution -d 2 \
                         -i Input/IXI002-Guys-0828-T2_slice90_resampled.nii.gz \
                         -j Input/IXI002-Guys-0828-T2_slice90.nii.gz \
@@ -14,9 +16,10 @@ NonLocalSuperResolution -d 2 \
 
 NonLocalSuperResolution -d 2 \
                         -i Input/IXI002-Guys-0828-T2_slice90_resampled.nii.gz \
-                        -j Input/IXI002-Guys-0828-T1_slice90.nii.gz \
+                        -k Input/IXI002-Guys-0828-T1_slice90.nii.gz \
                         -v 1 \
                         -s 32x16x8x4x2x1 \
                         -g 10.0 \
                         -t 1e5 \
-                        -o outputFromT1.nii.gz
+                        -o outputFromT1.nii.gz \
+                        -n bspline
